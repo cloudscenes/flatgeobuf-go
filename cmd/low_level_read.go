d3ebf25bd819d3e5695ff7eef10849ce54edd915
Nuno Lopes <nlopes@gmail.com>
2022-01-06 14:49:51 +0000

    Isolate offset calculations

--- cmd/low_level_read.go
+++ cmd/low_level_read.go
@@ -30,6 +30,43 @@ func calcTreeSize(numItems uint64, nodeSize uint16) uint32 {
 	return uint32(numNodes * NODE_ITEM_LEN)
 }
 
+type FGBReader struct {
+	b              []byte
+	indexOffset    uint32
+	featuresOffset flatbuffers.UOffsetT
+}
+
+func NewFGBReader(b []byte) (*FGBReader, error) {
+	fmt.Printf("magic bytes %x\n", b[:8])
+	//TODO: check magic bytes
+
+	res := FGBReader{b: b}
+
+	headerLength := binary.LittleEndian.Uint32(b[8:12])
+	var indexLength uint32
+
+	header := FlatGeobuf.GetSizePrefixedRootAsHeader(b, 8)
+	if header.IndexNodeSize() != 0 {
+		indexLength = calcTreeSize(header.FeaturesCount(), header.IndexNodeSize())
+	}
+
+	res.indexOffset = 8 + 4 + headerLength
+	res.featuresOffset = flatbuffers.UOffsetT(8 + 4 + headerLength + indexLength)
+
+	// TODO: should we validate the size?
+	//featureLen := binary.LittleEndian.Uint32(b[res.featuresOffset : res.featuresOffset+4])
+
+	return &res, nil
+}
+
+func (fgb *FGBReader) Header() *FlatGeobuf.Header {
+	return FlatGeobuf.GetSizePrefixedRootAsHeader(fgb.b, 8)
+}
+
+func (fgb *FGBReader) Feature() *FlatGeobuf.Feature {
+	return FlatGeobuf.GetSizePrefixedRootAsFeature(fgb.b, fgb.featuresOffset)
+}
+
 func main() {
 	f, err := os.Open("test/data/countries.fgb")
 	if err != nil {
@@ -41,14 +78,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// read format
-	fmt.Printf("magic bytes %x\n", b[:8])
-	//TODO: check format
+	fgb, err := NewFGBReader(b)
 
-	// READ Header
-	calcLen := binary.LittleEndian.Uint32(b[8:12])
-	header := FlatGeobuf.GetSizePrefixedRootAsHeader(b, 8)
-	fmt.Println("len", calcLen)
+	header := fgb.Header()
 	crs := FlatGeobuf.Crs{}
 	header.Crs(&crs)
 	fmt.Println("crs", string(crs.Name()), string(crs.Wkt()))
@@ -65,19 +97,8 @@ func main() {
 	}
 	fmt.Println("index", header.IndexNodeSize())
 
-	// READ Index
-	//TODO: check if there is an index
-	indexLength := calcTreeSize(header.FeaturesCount(), header.IndexNodeSize())
-	fmt.Println("index length", indexLength)
-	//TODO: parse index
-
-	offset := 8 + 4 + flatbuffers.UOffsetT(calcLen) + flatbuffers.UOffsetT(indexLength)
-	fmt.Println("offset", offset)
-
 	// READ features
-	featureLen := binary.LittleEndian.Uint32(b[offset : offset+4])
-	fmt.Println("featlen", featureLen)
-	features := FlatGeobuf.GetSizePrefixedRootAsFeature(b, offset)
+	features := fgb.Feature()
 	fmt.Println("col len", features.ColumnsLength())
 	fmt.Println("prop len", features.PropertiesLength())
 	fmt.Println("prop bytes", string(features.PropertiesBytes()))
