b8f4db16473a9e8fe313bf13b7abd5a48b44521a
Alexandre Silva <amsilva@infoportugal.impresa.pt>
2022-02-04 16:48:05 +0000

    Add more use cases for version getter tests

--- flatgeobuffer_test.go
+++ flatgeobuffer_test.go
@@ -16,18 +16,16 @@ func TestVersion(t *testing.T) {
 		wantErr error
 	}{
 		{name: "Valid", args: args{fileMagicBytes: []byte{0x66, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x62, 0x01}}, want: fmt.Sprintf("%d.0.1", supportedVersion), wantErr: nil},
+		{name: "Valid 2", args: args{fileMagicBytes: []byte{0x66, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x62, 0x08}}, want: fmt.Sprintf("%d.0.8", supportedVersion), wantErr: nil},
 		{name: "Invalid Bytes", args: args{fileMagicBytes: []byte{0x99, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x62, 0x01}}, want: "", wantErr: ErrInvalidFile},
+		{name: "Invalid Bytes 2", args: args{fileMagicBytes: []byte{0x66, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x31, 0x01}}, want: "", wantErr: ErrInvalidFile},
 		{name: "Unsupported Version", args: args{fileMagicBytes: []byte{0x66, 0x67, 0x62, 2, 0x66, 0x67, 0x62, 0x01}}, want: "", wantErr: ErrUnsupportedVersion},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := Version(tt.args.fileMagicBytes)
-			if err != tt.wantErr {
-				t.Errorf("Version() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("Version() got = %v, want %v", got, tt.want)
+			if err != tt.wantErr || got != tt.want {
+				t.Errorf("Version() got = %v, want %v, error = %v, wantErr %v", got, tt.want, err, tt.wantErr)
 			}
 		})
 	}
