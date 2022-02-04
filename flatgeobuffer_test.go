10f43f89fa8869bab7731bf91d68f07e70ee3424
Alexandre Silva <amsilva@infoportugal.impresa.pt>
2022-02-04 16:49:49 +0000

    Simplify tests for version getter

--- flatgeobuffer_test.go
+++ flatgeobuffer_test.go
@@ -6,24 +6,21 @@ import (
 )
 
 func TestVersion(t *testing.T) {
-	type args struct {
-		fileMagicBytes []byte
-	}
 	tests := []struct {
 		name    string
-		args    args
+		args    []byte
 		want    string
 		wantErr error
 	}{
-		{name: "Valid", args: args{fileMagicBytes: []byte{0x66, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x62, 0x01}}, want: fmt.Sprintf("%d.0.1", supportedVersion), wantErr: nil},
-		{name: "Valid 2", args: args{fileMagicBytes: []byte{0x66, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x62, 0x08}}, want: fmt.Sprintf("%d.0.8", supportedVersion), wantErr: nil},
-		{name: "Invalid Bytes", args: args{fileMagicBytes: []byte{0x99, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x62, 0x01}}, want: "", wantErr: ErrInvalidFile},
-		{name: "Invalid Bytes 2", args: args{fileMagicBytes: []byte{0x66, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x31, 0x01}}, want: "", wantErr: ErrInvalidFile},
-		{name: "Unsupported Version", args: args{fileMagicBytes: []byte{0x66, 0x67, 0x62, 2, 0x66, 0x67, 0x62, 0x01}}, want: "", wantErr: ErrUnsupportedVersion},
+		{name: "Valid", args: []byte{0x66, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x62, 0x01}, want: fmt.Sprintf("%d.0.1", supportedVersion), wantErr: nil},
+		{name: "Valid 2", args: []byte{0x66, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x62, 0x08}, want: fmt.Sprintf("%d.0.8", supportedVersion), wantErr: nil},
+		{name: "Invalid Bytes", args: []byte{0x99, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x62, 0x01}, want: "", wantErr: ErrInvalidFile},
+		{name: "Invalid Bytes 2", args: []byte{0x66, 0x67, 0x62, supportedVersion, 0x66, 0x67, 0x31, 0x01}, want: "", wantErr: ErrInvalidFile},
+		{name: "Unsupported Version", args: []byte{0x66, 0x67, 0x62, 2, 0x66, 0x67, 0x62, 0x01}, want: "", wantErr: ErrUnsupportedVersion},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Version(tt.args.fileMagicBytes)
+			got, err := Version(tt.args)
 			if err != tt.wantErr || got != tt.want {
 				t.Errorf("Version() got = %v, want %v, error = %v, wantErr %v", got, tt.want, err, tt.wantErr)
 			}
