926599ed41c0695f45b25f8c28e96bffe6018570
Nuno Lopes <nlopes@gmail.com>
2022-05-17 16:04:47 +0100

    Add tests for tree size calculation

--- index/packedRTree_test.go
+++ index/packedRTree_test.go
@@ -15,6 +15,8 @@ func TestCalcTreeSize(t *testing.T) {
 	}{
 		{name: "small", args: args{numItems: 2, nodeSize: 16}, want: 120, wantErr: false},
 		{name: "medium", args: args{numItems: 2000, nodeSize: 16}, want: 85360, wantErr: false},
+		{name: "not enough items", args: args{numItems: 0, nodeSize: 16}, want: 123456, wantErr: true},
+		{name: "node size too small", args: args{numItems: 20, nodeSize: 1}, want: 123456, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -23,7 +25,7 @@ func TestCalcTreeSize(t *testing.T) {
 				t.Errorf("CalcTreeSize() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if got != tt.want {
+			if err == nil && got != tt.want {
 				t.Errorf("CalcTreeSize() got = %v, want %v", got, tt.want)
 			}
 		})
