6e0837e0189011a8c9c556c8a92bf0ef988ce0ba
Nuno Lopes <nlopes@gmail.com>
2022-05-20 19:17:24 +0100

    Add tests to index search and some more fgb test files

--- flatgeobuffer_test.go
+++ flatgeobuffer_test.go
@@ -2,6 +2,10 @@ package flatgeobuf_go
 
 import (
 	"fmt"
+	"github.com/twpayne/go-geom"
+	"io"
+	"os"
+	"reflect"
 	"testing"
 )
 
@@ -23,6 +27,76 @@ func TestVersion(t *testing.T) {
 			got, err := Version(tt.args)
 			if err != tt.wantErr || got != tt.want {
 				t.Errorf("Version() got = %v, want %v, error = %v, wantErr %v", got, tt.want, err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func searchFGB(file string, box []float64) ([]geom.T, []geom.T, error) {
+	f, err := os.Open(file)
+	defer f.Close()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	b, err := io.ReadAll(f)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	fgb, err := NewFGBReader(b)
+	if err != nil {
+		return nil, nil, err
+	}
+	header := fgb.Header()
+	features := fgb.Features()
+	searchResult := fgb.Index().Search(box[0], box[1], box[2], box[3])
+	searchGeoms := make([]geom.T, len(searchResult))
+	for i, v := range searchResult {
+		feature := features.ReadAt(v.Offset)
+		g, _ := ParseGeometry(header, feature.Geometry(nil))
+		searchGeoms[i] = g
+	}
+
+	seqGeoms := make([]geom.T, 0)
+	filterBounds := geom.NewBounds(geom.XY).Set(box...)
+	for features.Next() {
+		feature := features.Read()
+		g, _ := ParseGeometry(header, feature.Geometry(nil))
+		gBounds := geom.NewBounds(geom.XY).Extend(g)
+		if gBounds.Overlaps(geom.XY, filterBounds) {
+			seqGeoms = append(seqGeoms, g)
+		}
+	}
+
+	return searchGeoms, seqGeoms, nil
+}
+
+func Test_Search(t *testing.T) {
+	tests := []struct {
+		name      string
+		file      string
+		searchBox []float64
+	}{
+		{name: "small no results", file: "test/data/simple-small.fgb", searchBox: []float64{-5, -5, -2, -2}},
+		{name: "small some results", file: "test/data/simple-small.fgb", searchBox: []float64{1, 1, 2, 2}},
+		{name: "small all results", file: "test/data/simple-small.fgb", searchBox: []float64{-1, -1, 20, 20}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			searchRes, seqRes, err := searchFGB(tt.file, tt.searchBox)
+			if err != nil {
+				t.Errorf("got unexpect error %v", err)
+				return
+			}
+			if len(searchRes) != len(seqRes) {
+				t.Errorf("Search len() = %d, want %d", len(searchRes), len(seqRes))
+				return
+			}
+			for i, v := range searchRes {
+				if !reflect.DeepEqual(v, seqRes[i]) {
+					t.Errorf("search geometries = %v, want %v", v, seqRes[i])
+				}
 			}
 		})
 	}
