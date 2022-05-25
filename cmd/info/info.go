00ce299ae05eb027d64c8fc73f96f94ee102c5ae
Nuno Lopes <nlopes@gmail.com>
2022-05-25 16:07:10 +0100

    Add a small info utility that allows benchmarks and performance testing

---
+++ cmd/info/info.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	flatgeobuf_go "flatgeobuf-go"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+func main() {
+	info := flag.Bool("info", false, "get info")
+
+	f, err := os.Create("/tmp/fgb_info.pprof")
+	if err != nil {
+		log.Fatal("could not create CPU profile: ", err)
+	}
+	defer f.Close()
+	if err = pprof.StartCPUProfile(f); err != nil {
+		log.Fatal("could not start CPU profile: ", err)
+	}
+	defer pprof.StopCPUProfile()
+
+	flag.Parse()
+	if len(flag.Args()) != 1 {
+		fmt.Println("please give a filename")
+		return
+	}
+
+	file := flag.Args()[0]
+	if stat, err := os.Stat(file); err != nil || stat.IsDir() {
+		fmt.Printf("file %s not found", file)
+		return
+	}
+
+	f, err = os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := io.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO: implement info
+	fmt.Println(*info)
+
+	fgb, err := flatgeobuf_go.NewFGBReader(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	features := fgb.Features()
+	for features.Next() {
+		geom, _ := features.ReadGeometry()
+
+		if geom == nil {
+			continue
+		}
+	}
+}
