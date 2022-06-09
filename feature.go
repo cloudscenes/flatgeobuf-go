439518786a4905581a3654ddf302c76178aae063
Alexandre Silva <amsilva@infoportugal.impresa.pt>
2022-06-09 15:53:34 +0100

    WIP: Unmarshall feature to custom struct

--- feature.go
+++ feature.go
@@ -4,6 +4,7 @@ import (
 	"flatgeobuf-go/FlatGeobuf"
 	"fmt"
 	"github.com/twpayne/go-geom"
+	"reflect"
 )
 
 type Feature struct {
@@ -57,4 +58,38 @@ func (f *Feature) Properties() map[string]interface{} {
 	props := f.features.propertyDecoder.Decode(f.fgbFeature.PropertiesBytes())
 
 	return props
+}
+
+func (f *Feature) Unmarshal(v interface{}) error {
+	rv := reflect.ValueOf(v)
+
+	if rv.Kind() != reflect.Ptr || rv.IsNil() || rv.Elem().Kind() != reflect.Struct {
+		return fmt.Errorf("cannot unmarshal")
+	}
+
+	props := f.Properties()
+
+	rt := rv.Elem().Type()
+	for i := 0; i < rt.NumField(); i++ {
+		field := rt.Field(i)
+
+		target := field.Name
+		if field.Tag != "" {
+			target = field.Tag.Get("fgb")
+		}
+
+		value, exists := props[target]
+		if !exists {
+			continue
+		}
+
+		val := reflect.ValueOf(value)
+		if val.Type() != field.Type {
+			panic("cannot unmarshal " + val.Type().Name() + " into Go struct field " + rt.Name() + "." + field.Name + " of type " + field.Type.Name())
+		}
+
+		rv.Elem().Field(i).Set(reflect.ValueOf(value))
+	}
+
+	return nil
 }
