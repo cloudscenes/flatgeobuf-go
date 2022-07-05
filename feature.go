25569e71c645643eb0160249f84943593d3cd0c3
Alexandre Silva <amsilva@infoportugal.impresa.pt>
2022-07-05 15:19:48 +0100

    11 unmarshal feature (#14)
    * WIP: Unmarshall feature to custom struct
    
    * Fix: when field has other tags other than fgb, target key was empty
    
    * Use lowercase title to fetch feature props in Unmarshal
    
    * Add tests for Unmarshal
    
    * Add unmarshal of geometry in Feature struct
    
    * Add test for geom tag unmarshal
    
    * Replace var to determine interface type by a nil cast value
    
    * Remove redundant check and typo in condition
    
    * Add comments to code

--- feature.go
+++ feature.go
@@ -4,6 +4,8 @@ import (
 	"flatgeobuf-go/FlatGeobuf"
 	"fmt"
 	"github.com/twpayne/go-geom"
+	"reflect"
+	"strings"
 )
 
 type Feature struct {
@@ -57,4 +59,62 @@ func (f *Feature) Properties() map[string]interface{} {
 	props := f.features.propertyDecoder.Decode(f.fgbFeature.PropertiesBytes())
 
 	return props
+}
+
+func (f *Feature) Unmarshal(v interface{}) error {
+	rv := reflect.ValueOf(v)
+
+	if rv.Kind() != reflect.Ptr || rv.IsNil() || rv.Elem().Kind() != reflect.Struct {
+		return fmt.Errorf("cannot unmarshal")
+	}
+
+	// TODO: handle error
+	geometry, _ := f.Geometry()
+	props := f.Properties()
+
+	rt := rv.Elem().Type()
+	gt := reflect.TypeOf(geometry)
+	for i := 0; i < rt.NumField(); i++ {
+		field := rt.Field(i)
+		fieldType := field.Type
+		fieldKind := fieldType.Kind()
+
+		target := field.Name
+		tag := field.Tag.Get("fgb")
+		if tag != "" {
+			target = tag
+		}
+
+		target = strings.ToLower(target)
+
+		if target == "geom" {
+			// only accept values that are geom.T
+			if fieldKind != reflect.Ptr && fieldType != reflect.TypeOf((*geom.T)(nil)).Elem() {
+				panic("cannot unmarshall geometry to a struct field that isn't a pointer or a geom.T interface!")
+			}
+
+			// don't try to match different types (e.g. store a Point in a Linestring var)
+			if fieldKind == reflect.Ptr && fieldType.Elem() != gt.Elem() {
+				panic("cannot unmarshal " + gt.Elem().Name() + " into Go struct field " + rt.Name() + "." + field.Name + " of type " + field.Type.Elem().Name())
+			}
+
+			rv.Elem().Field(i).Set(reflect.ValueOf(geometry))
+
+			continue
+		}
+
+		value, exists := props[target]
+		if !exists {
+			continue
+		}
+
+		val := reflect.ValueOf(value)
+		if val.Type() != field.Type {
+			panic("cannot unmarshal " + val.Type().Name() + " into Go struct field " + rt.Name() + "." + field.Name + " of type " + field.Type.Name())
+		}
+
+		rv.Elem().Field(i).Set(reflect.ValueOf(value))
+	}
+
+	return nil
 }
---
+++ feature_test.go
@@ -0,0 +1,88 @@
+package flatgeobuf_go
+
+import (
+	"github.com/twpayne/go-geom"
+	"log"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestUnmarshal(t *testing.T) {
+	f, err := os.Open("test/data/alldatatypes.fgb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fgb, err := NewFGB(f)
+
+	features := fgb.Features()
+	features.Next()
+	feature := features.Read()
+	props := feature.Properties()
+
+	type OutStruct struct {
+		Geom     geom.T
+		GeomCopy *geom.Point `fgb:"geom"`
+		Binary   []uint8
+		Boolean  bool `fgb:"bool"`
+		Byte     int8
+		Datetime time.Time
+		Double   float64
+		Float    float32
+		Int      int32
+		Json     string
+		Long     int64
+		Short    int16
+		Str      string `fgb:"string"`
+		Ubyte    uint8
+		Uint     uint32
+		Ulong    uint64
+		Ushort   uint16
+	}
+
+	tests := []struct {
+		name  string
+		props map[string]interface{}
+		want  *OutStruct
+	}{
+		{
+			name:  "alldatatypes",
+			props: props,
+			want: &OutStruct{
+				Geom:     geom.NewPointFlat(geom.XY, []float64{0, 0}),
+				GeomCopy: geom.NewPointFlat(geom.XY, []float64{0, 0}),
+				Binary:   []uint8{0x58},
+				Boolean:  true,
+				Byte:     int8(-1),
+				Datetime: time.Date(2020, time.February, 29, 12, 34, 56, 0, time.UTC),
+				Double:   float64(0),
+				Float:    float32(0),
+				Int:      int32(-1),
+				Json:     "X",
+				Long:     int64(-1),
+				Short:    int16(-1),
+				Str:      "X",
+				Ubyte:    uint8(0xff),
+				Uint:     uint32(0xffffffff),
+				Ulong:    uint64(0xffffffffffffffff),
+				Ushort:   uint16(0xffff),
+			},
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := &OutStruct{}
+
+			err := feature.Unmarshal(got)
+			if err != nil {
+				panic(err)
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Unmarshal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
