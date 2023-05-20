fc11efe604a71bfc58fc869825e5da832db38864
Nuno Lopes <nlopes@gmail.com>
2023-05-21 00:11:10 +0100

    feat: add property value encoding

--- properties.go
+++ properties.go
@@ -94,3 +94,125 @@ func (f *Feature) decodeVal(b []byte, col *FlatGeobuf.ColumnT) (interface{}, uin
 	}
 	return val, size
 }
+
+func (f *Feature) encodeVal(val interface{}, col *FlatGeobuf.ColumnT) ([]byte, error) {
+	var res []byte
+	var ok bool
+	switch col.Type {
+	case FlatGeobuf.ColumnTypeByte, FlatGeobuf.ColumnTypeUByte:
+		var v byte
+		v, ok = val.(byte)
+		if !ok {
+			break
+		}
+		res = make([]byte, flatbuffers.SizeByte)
+		flatbuffers.WriteByte(res, v)
+	case FlatGeobuf.ColumnTypeBool:
+		var b bool
+		b, ok = val.(bool)
+		if !ok {
+			break
+		}
+		res = make([]byte, flatbuffers.SizeBool)
+		flatbuffers.WriteBool(res, b)
+	case FlatGeobuf.ColumnTypeShort:
+		var v int16
+		v, ok = val.(int16)
+		if !ok {
+			break
+		}
+		res = make([]byte, flatbuffers.SizeInt16)
+		flatbuffers.WriteInt16(res, v)
+	case FlatGeobuf.ColumnTypeUShort:
+		var v uint16
+		v, ok = val.(uint16)
+		if !ok {
+			break
+		}
+		res = make([]byte, flatbuffers.SizeUint16)
+		flatbuffers.WriteUint16(res, v)
+	case FlatGeobuf.ColumnTypeInt:
+		var v int32
+		v, ok = val.(int32)
+		if !ok {
+			break
+		}
+		res = make([]byte, flatbuffers.SizeUint32)
+		flatbuffers.WriteInt32(res, v)
+	case FlatGeobuf.ColumnTypeUInt:
+		var v uint32
+		v, ok = val.(uint32)
+		if !ok {
+			break
+		}
+		res = make([]byte, flatbuffers.SizeUint32)
+		flatbuffers.WriteUint32(res, v)
+	case FlatGeobuf.ColumnTypeLong:
+		var v int64
+		v, ok = val.(int64)
+		if !ok {
+			break
+		}
+		res = make([]byte, flatbuffers.SizeInt64)
+		flatbuffers.WriteInt64(res, v)
+	case FlatGeobuf.ColumnTypeULong:
+		var v uint64
+		v, ok = val.(uint64)
+		if !ok {
+			break
+		}
+		res = make([]byte, flatbuffers.SizeUint64)
+		flatbuffers.WriteUint64(res, v)
+	case FlatGeobuf.ColumnTypeFloat:
+		var v float32
+		v, ok = val.(float32)
+		if !ok {
+			break
+		}
+		res = make([]byte, flatbuffers.SizeFloat64)
+		flatbuffers.WriteFloat32(res, v)
+	case FlatGeobuf.ColumnTypeDouble:
+		var v float64
+		v, ok = val.(float64)
+		if !ok {
+			break
+		}
+		res = make([]byte, flatbuffers.SizeFloat64)
+		flatbuffers.WriteFloat64(res, v)
+	case FlatGeobuf.ColumnTypeString, FlatGeobuf.ColumnTypeJson:
+		var str string
+		str, ok = val.(string)
+		if !ok {
+			break
+		}
+		strSize := uint32(len(str))
+		res = make([]byte, varDataSize+strSize)
+		flatbuffers.WriteUint32(res, strSize)
+		copy(res[varDataSize:], str)
+	case FlatGeobuf.ColumnTypeDateTime:
+		var t time.Time
+		t, ok = val.(time.Time)
+		if !ok {
+			break
+		}
+		str := t.Format(time.RFC3339)
+		strSize := uint32(len(str))
+		res = make([]byte, varDataSize+strSize)
+		flatbuffers.WriteUint32(res, strSize)
+		copy(res[varDataSize:], str)
+	case FlatGeobuf.ColumnTypeBinary:
+		var b []byte
+		b, ok = val.([]byte)
+		if !ok {
+			break
+		}
+		bSize := uint32(len(b))
+		flatbuffers.WriteUint32(res, bSize)
+		copy(res[varDataSize:], b)
+	}
+
+	if !ok {
+		return nil, fmt.Errorf("invalid value type")
+	}
+	return res, nil
+}
